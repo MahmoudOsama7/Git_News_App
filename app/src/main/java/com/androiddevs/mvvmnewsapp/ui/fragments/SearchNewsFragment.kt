3e3758775906c792edf024756ec94dadada33d45
Mahmoud Osama <mahmoud@34ml.com>
2022-12-11 17:53:30 +0200

    redesigning the methods

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.ui.NewsActivity
@@ -18,40 +19,45 @@ import kotlinx.android.synthetic.main.fragment_breaking_news.*
 import kotlinx.android.synthetic.main.fragment_search_news.*
 import kotlinx.android.synthetic.main.fragment_search_news.paginationProgressBar
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.MainScope
+
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import retrofit2.http.Tag
 
 class SearchNewsFragment:Fragment(R.layout.fragment_search_news) {
     lateinit var viewModel: NewsViewModel
     lateinit var searchNewsAdapter: NewsAdapter
     private val TAG="SearchgNewsFragment"
-
-
-
-
+    var job:Job?=null
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel= (activity as NewsActivity).viewModel
         setupRecyclerView()
+        setupSearch()
+        loadData()
 
-        var job:Job?=null
-        etSearch.addTextChangedListener {editable->
-            job?.cancel()
-            job= MainScope().launch {
-                delay(SEARCH_NEWS_TIME_DELAY)
-                editable.let {
-                    if(editable.toString().isNotEmpty()){
-                        viewModel.getSearchNews(editable.toString())
-                    }
-                }
-            }
+
+    }
+    private fun hideProgressBar() {
+        rvSearchNews.visibility=View.VISIBLE
+        paginationProgressBar.visibility=View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        rvSearchNews.visibility=View.INVISIBLE
+        paginationProgressBar.visibility=View.VISIBLE
+    }
+
+    //setupRecyclerView
+    private fun setupRecyclerView(){
+        searchNewsAdapter= NewsAdapter()
+        rvSearchNews.apply {
+            adapter=searchNewsAdapter
+            layoutManager= LinearLayoutManager(activity)
         }
-
-
+    }
+    private fun loadData(){
         viewModel.searchNews.observe(viewLifecycleOwner, Observer {response->
             when(response){
                 is Resource.Success->{
@@ -72,22 +78,17 @@ class SearchNewsFragment:Fragment(R.layout.fragment_search_news) {
             }
         })
     }
-    private fun hideProgressBar() {
-        rvSearchNews.visibility=View.VISIBLE
-        paginationProgressBar.visibility=View.INVISIBLE
-    }
-
-    private fun showProgressBar() {
-        rvSearchNews.visibility=View.INVISIBLE
-        paginationProgressBar.visibility=View.VISIBLE
-    }
-
-    //setupRecyclerView
-    private fun setupRecyclerView(){
-        searchNewsAdapter= NewsAdapter()
-        rvSearchNews.apply {
-            adapter=searchNewsAdapter
-            layoutManager= LinearLayoutManager(activity)
+    private fun setupSearch(){
+        etSearch.addTextChangedListener {editable->
+            job?.cancel()
+            job= lifecycleScope.launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable.let {
+                    if(editable.toString().isNotEmpty()){
+                        viewModel.getSearchNews(editable.toString())
+                    }
+                }
+            }
         }
     }
 }
