c1121dc40604e40822ce37a53907e1a49b48b0e2
Mahmoud Osama <mahmoud@34ml.com>
2022-12-28 10:50:48 +0200

    adding all probabilities to order delivery

--- app/src/androidTest/java/com/androiddevs/mvvmnewsapp/NewsViewModelTest.kt
+++ app/src/androidTest/java/com/androiddevs/mvvmnewsapp/NewsViewModelTest.kt
@@ -1,4 +1,13 @@
 package com.androiddevs.mvvmnewsapp
 
-class NewsViewModelTest {
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+/**
+ * we created a class calledNewsViewModelTest to test the NewsViewModel class , and we annotated it with @runwith
+ * (AndroidJUnit4) not Junit4 directly
+ * as this is android component test , not normal tetss for the logic that is put in test folders
+ */
+@RunWith(AndroidJUnit4::class)
+class NewsViewModelTest:Te {
 }
